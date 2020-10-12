53e464a3d8288b26ee3014593d82d9187033e696
zangshoujin <61638229+zangshoujin@users.noreply.github.com>
2020-10-12 17:11:14 +0800

    git bug

--- src/print.h
+++ src/print.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef __print_h__
 #define __print_h__
 typedef unsigned char byte;
@@ -18,25 +17,4 @@ int print_encrypt_num(int first_encrypt_num[],int all_encrypt_num[],int later_fa
 
 int print_round_key(byte in[16],byte out[16],byte key[16],byte outex[16],int nt,int base,byte w[176]);
 
-=======
-#ifndef __print_h__
-#define __print_h__
-typedef unsigned char byte;
-
-#include "encrypt.h"
-
-void print_4_by_4(byte temp[16]);
-void print_4_by_4_int(int temp[16]);
-int Is_equal(byte a[16],byte b[16]);
-int print_count(int first_success_num,int first_fail_num,int first_out_time_num,int success_num_in_fail[],int fail_num_in_fail[],
-	int out_time_num_in_fail[],int success_num_in_out_time[],int fail_num_in_out_time[],
-	int out_time_num_in_out_time[],int other_fail_num,int no_chain_num,int more_chain_num,int one_chain_num,int invalid_error_num,
-	int success_num_in_timeout,int fail_num_in_timeout,int timeout_num_in_timeout);
-
-int print_encrypt_num(int first_encrypt_num[],int all_encrypt_num[],int later_fail_encrypt_num[][Experment_num],
-	int later_out_time_encrypt_num[][Experment_num]);
-
-int print_round_key(byte in[16],byte out[16],byte key[16],byte outex[16],int nt,int base,byte w[176]);
-
->>>>>>> 5ecc1506ac1b2d8658c0e0d7caad6cb613c4a7c7
 #endif
--- src/verify.h
+++ src/verify.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef __verify_h__
 #define __verify_h__
 typedef int bool;
@@ -33,40 +32,4 @@ int verify_offline_key(byte guess_key_10round[16][16],byte key_10round[16],byte 
 	int *first_out_time_num,int *other_fail_num);
 
 
-=======
-#ifndef __verify_h__
-#define __verify_h__
-typedef int bool;
-#define true 1
-#define false 0
-typedef unsigned char byte;
-#include "filter.h"
-#include "difftribute_table.h"
-#include "aes_rp.h"
-#include "recovery.h"
-#include "aes_htable.h"
-#include "aes_share.h"
-#include <stdio.h>
-
-#define timeout_Num 4194304
-/*
-	2的30次方1073741824
-	2的20次方1048576
-	2的22次方4194304
-	2的23次方8388608
-	24:
-	2的25次方33554432 理论上这个的超时时间应该是1800秒
-*/
-
-int verify_online_key(byte guess_key_10round[16][16],byte key_10round[16],byte w[176],int candidiate_key_count[16],
-	int* first_success_num,int* first_fail_num,byte cipher_verify[16],byte in[16],int n,int nt,int base,byte reall_main_key[16],
-	int *first_out_time_num,int *other_fail_num);
-
-
-int verify_offline_key(byte guess_key_10round[16][16],byte key_10round[16],byte w[176],int candidiate_key_count[16],
-	int* first_success_num,int* first_fail_num,byte cipher_verify[16],byte in[16],int n,int nt,int base,byte reall_main_key[16],
-	int *first_out_time_num,int *other_fail_num);
-
-
->>>>>>> 5ecc1506ac1b2d8658c0e0d7caad6cb613c4a7c7
 #endif
